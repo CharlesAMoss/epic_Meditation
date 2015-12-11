9040a9e63e1cf3e2ebbb5a7da6137f529760367e
CharlesAMoss <charlesamoss@yahoo.com>
2015-12-11 14:53:47 -0800

    initial commit

---
+++ app/src/main/java/example/epic/myapp/MainActivity.java
@@ -0,0 +1,33 @@
+package example.epic.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.mainButton);
+        final EditText editText = (EditText) findViewById(R.id.nameEditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                    Intent intent = new Intent(MainActivity.this, SetTime.class);
+                    startActivity(intent);
+
+
+                Log.d("click","hello editText this is the on click");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/example/epic/myapp/SetTime.java
@@ -0,0 +1,21 @@
+package example.epic.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by charlesamoss on 12/11/15.
+ */
+public class SetTime extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+
+    }
+}
