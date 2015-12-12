6deefac5ee51ed9b751adeff51545ba8a18fd397
CharlesAMoss <charlesamoss@yahoo.com>
2015-12-11 16:11:33 -0800

    add stats page

---
+++ app/src/main/java/example/epic/myapp/Stats.java
@@ -0,0 +1,22 @@
+package example.epic.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by charlesamoss on 12/11/15.
+ */
+public class Stats extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+
+    }
+
+}
