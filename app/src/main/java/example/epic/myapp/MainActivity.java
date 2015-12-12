1af706682110c7f5841330cc70ebb3027105e7e5
CharlesAMoss <charlesamoss@yahoo.com>
2015-12-11 16:43:50 -0800

    add fix to typo

--- app/src/main/java/example/epic/myapp/MainActivity.java
+++ app/src/main/java/example/epic/myapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;git
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toolbar;
